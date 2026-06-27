fae659ddb72eb581c27937be5a67982356bc9800
Dominic <wowschockadin@gmail.com>
2026-06-27 15:27:07 +0200

    no ai header fix

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,6 @@ const nextConfig: NextConfig = {
         source: "/home",
         destination: "/",
         permanent: true, // 308-Redirect – für SEO und Browser-Cache
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "noai, noimageai",
-          },
-        ],
       },
     ];
   },
